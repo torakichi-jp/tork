87cb60aa5cd0638a3afd6de5466bbd2594a99aa3
torakichi-jp <airothick@gmail.com>
2015-07-14 21:06:19 +0900

    weak_ptr.h 追加

--- include/tork/memory.h
+++ include/tork/memory.h
@@ -2,6 +2,7 @@
 #define TORK_MEMORY_H_INCLUDED
 
 #include "memory/shared_ptr.h"
+#include "memory/weak_ptr.h"
 #include "memory/default_deleter.h"
 #include "memory/allocator.h"
 
---
+++ include/tork/memory/weak_ptr.h
@@ -0,0 +1,28 @@
+﻿//******************************************************************************
+//
+// ウィークポインタ
+//
+//******************************************************************************
+
+#ifndef TORK_MEMORY_WEAK_PTR_H_INCLUDED
+#define TORK_MEMORY_WEAK_PTR_H_INCLUDED
+
+#include "shared_ptr.h"
+
+namespace tork {
+
+//==============================================================================
+// ウィークポインタ
+//==============================================================================
+template<class T>
+class weak_ptr {
+    impl::shared_holder_base* p_holder_ = nullptr;
+
+public:
+
+};  // class weak_ptr
+
+}   // namespace tork
+
+#endif  // TORK_MEMORY_WEAK_PTR_H_INCLUDED
+
