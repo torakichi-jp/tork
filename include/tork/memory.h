1e942dd2904150ba5e9a7d5f5bd874689fafcd64
torakichi-jp <airothick@gmail.com>
2015-07-15 09:29:16 +0900

    unique_ptr.h 追加

--- include/tork/memory.h
+++ include/tork/memory.h
@@ -1,6 +1,7 @@
 #ifndef TORK_MEMORY_H_INCLUDED
 #define TORK_MEMORY_H_INCLUDED
 
+#include "memory/unique_ptr.h"
 #include "memory/shared_ptr.h"
 #include "memory/weak_ptr.h"
 #include "memory/default_deleter.h"
---
+++ include/tork/memory/unique_ptr.h
@@ -0,0 +1,29 @@
+﻿//******************************************************************************
+//
+// 所有権移動スマートポインタ
+//
+//******************************************************************************
+#ifndef TORK_MEMORY_UNIQUE_PTR_INCLUDED
+#define TORK_MEMORY_UNIQUE_PTR_INCLUDED
+
+#include "default_deleter.h"
+
+namespace tork {
+
+    template<class T, class D = tork::default_deleter<T>>
+    class unique_ptr {
+    public:
+        typedef T* pointer;
+        typedef T element_type;
+        typedef D deleter_type;
+
+    private:
+        pointer ptr_;           // 保持するポインタ
+        deleter_type deleter_;  // 削除子
+
+    };  // class unique_ptr
+
+}   // namespace tork
+
+#endif  // TORK_MEMORY_UNIQUE_PTR_INCLUDED
+
