8a3f030b99d9832fd3bd1c1597d465e939421ad2
torakichi-jp <airothick@gmail.com>
2015-08-14 00:16:58 +0900

    algorithm.h 追加

---
+++ include/tork/algorithm.h
@@ -0,0 +1,22 @@
+﻿
+#ifndef TORK_ALGORITHM_H_INCLUDED
+#define TORK_ALGORITHM_H_INCLUDED
+
+namespace tork {
+
+// for_each() の添え字付き版
+template<class InputIter, class Function>
+Function each_with_index(InputIter first, InputIter last, Function f)
+{
+    size_t i = 0;
+    for (InputIter it = first; it != last; ++it) {
+        f(*it, i);
+        ++i;
+    }
+    return std::move(f);
+}
+
+}   // namespace tork
+
+#endif  // TORK_ALGORITHM_H_INCLUDED
+
